c39b19680ae7b4e99a635fbe447d8cc451b9b64a
Ngomory <ngomory14@gmail.com>
2023-05-01 10:15:28 +0000

    Package update

--- src/Helpy.php
+++ src/Helpy.php
@@ -5,19 +5,47 @@ namespace Ngomory;
 class Helpy
 {
 
-    static function formatDatetime(string $datetime, array $options = []): string
+    /**
+     * dateToLocal
+     *
+     * @param string|null $datetime String datetime value or null. can have the value 'now'.
+     * @param string $local String local code fr|en.
+     * @param boolean $time Display or not the date.
+     * @return string
+     */
+    public static function dateToLocal($datetime, string $local = 'fr', bool $showTime = true): string
     {
-
-        $format = $options['format'] ?? 'Y-m-d H:i:s';
-
-        if (empty($datetime)) {
-            return '';
-        } else {
-            return date($format, strtotime($datetime));
+        switch ($local) {
+            case 'fr':
+                $toFormat = 'd/m/Y' . ($showTime ? ' à H:i:s' : '');
+                return self::dateFormat($datetime, $toFormat);
+                break;
+            case 'en':
+                $toFormat = 'Y-m-d' . ($showTime ? ' at H:i:s' : '');
+                return self::dateFormat($datetime, $toFormat);
+                break;
+            default:
+                return '';
+                break;
         }
     }
 
-    static function formatNumber(float $number, array $option = [])
+    /**
+     * dateFormat
+     *
+     * @param string|null $datetime String datetime value or null. can have the value 'now'.
+     * @param string $fromFormat
+     * @param string $toFormat
+     * @return string
+     */
+    public static function dateFormat($datetime, $toFormat = 'd/m/Y H:i:s', $fromFormat = 'Y-m-d H:i:s'): string
+    {
+        $datetime = ($datetime == 'now') ? date('Y-m-d H:i:s') : $datetime;
+        $date = \DateTime::createFromFormat($fromFormat, $datetime);
+        return $date ? $date->format($toFormat) : '';
+    }
+
+    public static function formatNumber(float $number, array $option = [])
     {
 
         $decimals = (int)$option['decimals'] ?? 2;
@@ -27,18 +55,29 @@ class Helpy
         return number_format($number, $decimals, $decimal_separator, $thousands_separator);
     }
 
-    static function strRandom(int $length = 16, array $options = []): string
+    /**
+     * strRandom
+     *
+     * @param integer $length The length of the result
+     * @param array $options Possible options include | excluded
+     * @return string
+     */
+    public static function strRandom(int $length = 16, array $options = ['include' => '', 'excluded' => '']): string
     {
 
         $characters = $options['characters'] ?? '';
-
         if (empty($characters)) {
             $characters = '0123456789';
             $characters .= 'abcdefghijklmnopqrstuvwxyz';
             $characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         }
+        $characters = str_split($characters);
 
-        $charactersLength = strlen($characters);
+        $characters = array_merge($characters, str_split($options['include'] ?? ''));
+        $characters = array_diff($characters, str_split($options['excluded'] ?? ''));
+        shuffle($characters);
+
+        $charactersLength = count($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
             $randomString .= $characters[rand(0, $charactersLength - 1)];
